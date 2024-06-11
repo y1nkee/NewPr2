cbbdc89eeb158eb8a27715709bc522652c1e606d
Бугай Дмитрий Олегович <y1nkee@bk.ru>
2024-06-11 18:49:44 +0300

    Исправлено

--- main.go
+++ main.go
@@ -41,6 +41,9 @@ func (t Training) distance() float64 {
 // meanSpeed возвращает среднюю скорость бега или ходьбы.
 func (t Training) meanSpeed() float64 {
 	// вставьте ваш код ниже
+	if t.Duration == 0 {
+		return 0
+	}
 	return t.distance() / (float64(t.Duration.Minutes()) / MinsInHour)
 }
 
@@ -164,6 +167,9 @@ type Swimming struct {
 // Это переопределенный метод Calories() из Training.
 func (s Swimming) meanSpeed() float64 {
 	// вставьте ваш код ниже
+	if s.Duration == 0 {
+		return 0
+	}
 	return float64(s.LengthPool*s.CountPool) / MInKm / s.Duration.Hours()
 }
 
